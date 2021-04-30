34fd8b3d978c8fdc2d5c62d72034c37939a21a5f
Kanishka Mishra <kanishkamishra2000@gmail.com>
2021-04-30 12:49:53 +0530

    Final commit

---
+++ Trees/Binary Search Tree_using_Linkedlist.cpp
@@ -0,0 +1,242 @@
+//
+//  main.cpp
+//  DS Tree 2
+//
+//  Created by Kanishka Mishra on 4/6/21.
+//Write a menu driven program in C to a. Create a binary search tree b. search & Delete the node
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int num;
+
+class node{
+public:
+    node *left;
+    int data;
+    node *right;
+    node(){
+        left=right=NULL;
+    }
+};
+
+class BinarySearchTree:public node{
+    node* root;
+    node *search(node *root,int value);
+    void levelOrderTraversal(node *root);
+public:
+    void create();
+    node *search(int value);
+    void delete_node(int n);
+    bool printLevel(node *root,int level);
+    void levelOrderTraversal();
+    bool check_empty();
+};
+
+void BinarySearchTree::create(){
+    node *temp, *newnode;
+    int elm;
+    cout<<"Enter number of nodes: ";
+    cin>>num;
+    for(int i=0;i<num;i++){
+        newnode=new node();
+        cout<<"Enter data: ";
+        cin>>elm;
+        newnode->data=elm;
+        if(root==NULL){
+            root=newnode;
+        }
+        else{
+            temp=root;
+            while(1){
+                if(elm<temp->data){
+                    if(temp->left==NULL){
+                        temp->left=newnode;
+                        break;
+                    }
+                    else
+                        temp=temp->left;
+                }
+                else{
+                    if(temp->right==NULL){
+                        temp->right=newnode;
+                        break;
+                    }
+                    else
+                        temp=temp->right;
+                }
+            }
+        }
+    }
+}
+int search_level=0;
+node *BinarySearchTree::search(node *root,int value){
+    if(root!=NULL){
+        if(root->data==value){
+            cout<<"Level: "<<search_level<<endl;
+            return root;
+        }
+        if(value<root->data){
+            search_level++;
+            return search(root->left, value);
+        }
+        else{
+            search_level++;
+            return search(root->right, value);
+        }
+    }
+    return NULL;
+}
+
+node *BinarySearchTree::search(int value){
+    return search(root,value);
+}
+
+void BinarySearchTree::delete_node(int n){
+    node *temp, *parent=NULL,*father, *replace, *son;
+    temp=root;          //temp holds the node to be deleted
+    
+    //to search the node to be deleted
+    while((temp!=NULL) && (temp->data!=n)){
+        if(n<temp->data){   //to find if the data is in the left sub tree
+            parent=temp;    //parent points towards the parent of the node to be deleted
+            temp=temp->left;
+        }
+        else{
+            parent=temp;    //if data is in the right sub tree
+            temp=temp->right;
+        }
+    }
+    //if node is not present
+    if(temp==NULL){
+        cout<<"Number not found!\n"<<endl;
+        exit(1);
+    }
+    
+    //case: when the node has only right child
+    if(temp->left==NULL){
+        replace=temp->right;
+    }
+    //case: only left child
+    else if(temp->right==NULL)
+        replace=temp->left;
+    //case: has both children
+    else{
+        //finds the smallest element from the right sub tree of the node to replace
+        father=temp;
+        replace=temp->right;
+        son=replace->left;
+        while(son!=NULL){
+            father=replace;
+            replace=son;
+            son=replace->left;
+        }
+        if(father!=temp){
+            father->left=replace->right;
+            replace->right=temp->right;
+        }
+        //making the left part of the node to be replaced point to the left part of the node to be deleted. i.e. connecting left node of the node to be deleted to the replacable node
+        replace->left=temp->left;
+    }
+    
+    //substituting replace node at the position of the node being deleted
+    if(parent==NULL)    //if the node to be deleted is the root node
+        root=replace;
+    else{
+        if(temp==parent->left)
+            parent->left=replace;
+        else
+            parent->right=replace;
+    }
+    
+    //deletes the node
+    delete(temp);
+}
+
+bool BinarySearchTree::printLevel(node *root,int level){
+    if(root==NULL)
+        return false;
+    if(level==1){
+        cout<<root->data<<"  ";
+        return true;
+    }
+    bool left = printLevel(root->left,level-1);
+    bool right = printLevel(root->right,level-1);
+    
+    return left || right;   //or is used coz if only one child is present it still moves onto check the next level
+}
+
+void BinarySearchTree::levelOrderTraversal(node *root){
+    int level =1;
+    cout<<"Level "<<level-1<<" : ";
+    while(printLevel(root, level)){
+        cout<<"\nLevel "<<level<<" : ";
+        level++;
+    }
+    cout<<"NULL \n\n";
+}
+
+void BinarySearchTree::levelOrderTraversal(){
+    levelOrderTraversal(root);
+}
+
+bool BinarySearchTree::check_empty(){
+    if(root==NULL){
+        return true;
+    }
+    return false;
+}
+    
+int main(){
+
+    int ans,choice,value;
+    node *result;
+    BinarySearchTree bst;
+    do{
+        cout<<" 1: Create A binary Search Tree \n 2: Search an element\n 3: To delete a node \n";
+        cin>>choice;
+        switch(choice){
+            case 1:
+                bst.create();
+                cout<<"Tree successfully created! \n\n";
+                bst.levelOrderTraversal();
+                break;
+            case 2:
+                if(bst.check_empty()){
+                    cout<<"Tree is Empty!\n";
+                    break;
+                }
+                else{
+                    cout<<"Enter value you want to search: ";
+                    cin>>value;
+                    result=bst.search(value);
+                    if(result==NULL){
+                        cout<<"Element not found!\n";
+                    }
+                       else{
+                           cout<<"Element found! \n";
+                    }
+                    break;
+                }
+            case 3:
+                if(bst.check_empty()){
+                    cout<<"Tree is Empty!\n";
+                    break;
+                }
+                else{
+                    cout<<"Enter value you want to delete: ";
+                    cin>>value;
+                    bst.delete_node(value);
+                    cout<<"Node successfully deleted! \n\n";
+                    bst.levelOrderTraversal();
+                    break;
+                }
+            default:
+                cout<<"Inavlid Choice! \n";
+        }
+        cout<<"Run again: 1-YES, 0-NO \n";
+        cin>>ans;
+    }while(ans==1);
+}
---
+++ Trees/Binary Tree_using_Linked List.cpp
@@ -0,0 +1,346 @@
+//
+//  main.cpp
+//  DS Trees 1
+//
+//  Created by Kanishka Mishra on 2/24/21.
+//
+
+/* Write a menu driven program in C to
+ a. Create a binary tree
+ b. Recursive  and  non-recursive  tree  traversals:  inorder,  preorder  and post order */
+
+#include <iostream>
+
+using namespace std;
+int num;
+
+class node{
+public:
+    node *left;
+    char data;
+    node *right;
+    node(){
+        left=right=NULL;
+    }
+};
+
+class Tree:public node{
+    node *root;
+    void preorder(node * currentnode);
+    void inorder(node * currentnode);   //private declaration to keep the root node as private
+    void postorder(node * currentnode);
+public:
+    Tree(){
+        root=NULL;
+    }
+    void create();          //creates a tree
+    void preorder();
+    void inorder();
+    void postorder();
+    int check();            //to check if a tree is empty or not
+    void nonrec_preorder();
+    void nonrec_inorder();
+    void nonrec_postorder();
+    bool printLevel(node *root,int level);
+    void levelOrderTraversal(node *root);
+    void levelOrderTraversal();
+    
+};
+
+class stack{        //for non-recursive traversal
+    int top;
+    node* item[20];
+public:
+    stack(){
+        top=-1;
+    }
+    void push(node* p){
+        item[++top]=p;
+    }
+    bool isFull(){
+        if(top==num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    node* pop(){
+        if(isEmpty()){
+            cout<<"Stack is empty!";
+            return NULL;
+        }
+        else{
+            return item[top--];
+        }
+    }
+};
+
+int Tree::check(){
+    if(root==NULL){
+        return 1;
+    }
+    else
+        return 0;
+}
+
+void Tree::create(){
+    int num;
+    char elm;
+    char ans;
+    node *temp;
+    cout<<"Enter number of nodes: ";
+    cin>>num;
+    for(int i=0;i<num;i++){
+        node *new_node=new node();
+        cout<<"Enter data: ";
+        cin>>elm;
+        new_node->data=elm;
+        if(root==NULL){
+            root=new_node;
+        }
+        else{
+            temp=root;
+            while(1){
+                cout<<"\nEnter element at " <<temp->data<<"'s left or right (L/R) ? ";
+                cin>>ans;
+                if(ans == 'l' || ans == 'L' ){
+                    if(temp->left==NULL){
+                        temp->left=new_node;
+                        cout<<"Element added! \n\n";
+                        break;
+                    }
+                    else{
+                        temp=temp->left;
+                        cout<<"Node already present! "<<endl;
+                    }
+                }
+                else if(ans =='r' || ans == 'R'){
+                    if(temp->right==NULL){
+                        temp->right=new_node;
+                        cout<<"Element added! \n\n";
+                        break;
+                    }
+                    else{
+                        temp=temp->right;
+                        cout<<"Node already present! "<<endl;
+                    }
+                }
+                else{
+                    cout<<"Invalid choice! Choose L/R."<<endl;
+                }
+            }
+        }
+    }
+}
+
+void Tree::preorder(){  //recursive has internal stack
+    preorder(root);
+}
+
+void Tree::preorder(node * currentnode){
+    if(currentnode!=NULL){
+        cout<<currentnode->data;
+        cout<<"  ";
+        preorder(currentnode->left);
+        preorder(currentnode->right);
+    }
+}
+
+void Tree::inorder(){
+    inorder(root);
+}
+
+void Tree::inorder(node * currentnode){
+    if(currentnode!=NULL){
+        inorder(currentnode->left);
+        cout<<currentnode->data;
+        cout<<"  ";
+        inorder(currentnode->right);
+    }
+}
+
+void Tree::postorder(){
+    postorder(root);
+}
+
+void Tree::postorder(node * currentnode){
+    if(currentnode!=NULL){
+        postorder(currentnode->left);
+        postorder(currentnode->right);
+        cout<<currentnode->data;
+        cout<<"  ";
+    }
+}
+
+void Tree::nonrec_inorder(){
+    node *temp=root;
+    stack s;
+    cout<<"\nThe non-recursive in-order traversal is: \n";
+    while(1){
+        while(temp!=NULL){
+            s.push(temp);
+            temp=temp->left;    //traversing towards left
+        }
+        if(s.isEmpty()){
+            break;
+        }
+        temp = s.pop();
+        cout<<temp->data;
+        cout<<"  ";
+        temp=temp->right;
+    }
+}
+
+void Tree::nonrec_preorder(){
+    node *temp=root;
+    stack s;
+    cout<<"The non-recursive pre-order traversal is: \n";
+    while(1){
+        while(temp!=NULL){
+            cout<<temp->data;
+            cout<<"  ";
+            s.push(temp);
+            temp=temp->left;
+        }
+        if(s.isEmpty()){
+            break;
+            //cout<<"It is empty!";
+        }
+        temp = s.pop();
+        temp=temp->right;
+    }
+}
+
+void Tree::nonrec_postorder(){          //LRD
+    node *temp=root;
+    node *t;
+    stack s1,s2;
+    cout<<"The non-recursive post-order traversal is: \n";
+    s1.push(temp);
+    while(s1.isEmpty() == false){
+        temp=s1.pop();
+        s2.push(temp);
+        //pushing every root node's  left and right child onto stack 1
+        /* can not interchange left part push & right part push because in stack 2 first data is stored of right child and then left child*/
+        if(temp->left!=NULL){
+            s1.push(temp->left);
+        }
+        if(temp->right!=NULL){
+            s1.push(temp->right);
+        }
+    }
+    while(s2.isEmpty()==false){
+        t=s2.pop();
+        cout<<t->data;
+        cout<<"  ";
+    }
+}
+
+
+bool Tree::printLevel(node *root,int level){
+    if(root==NULL)
+        return false;
+    if(level==1){
+        cout<<root->data<<"  ";
+        return true;
+    }
+    bool left = printLevel(root->left,level-1);
+    bool right = printLevel(root->right,level-1);
+    
+    return left || right;   //or is used coz if only one child is present it still moves onto check the next level
+}
+
+void Tree::levelOrderTraversal(node *root){
+    int level =1;
+    cout<<"Level "<<level-1<<" : ";
+    while(printLevel(root, level)){
+        cout<<"\nLevel "<<level<<" : ";
+        level++;
+    }
+    cout<<"NULL \n\n";
+}
+
+void Tree::levelOrderTraversal(){
+    levelOrderTraversal(root);
+}
+
+int main() {
+    int choice,ans;
+    Tree tree;
+    do{
+        cout<<"\n 1: To create Tree \n 2: To traverse Tree recursively \n 3: To traverse tree non-recursively \n"<<endl;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                tree.create();
+                tree.levelOrderTraversal();
+                break;
+            case 2:
+                if(tree.check()){
+                    cout<<"Tree is Empty! \n";
+                }
+                else{
+                    do{
+                        cout<<"\t 1: Preorder \n\t 2: Inorder \n\t 3: Postorder \n"<<endl;
+                        cin>>choice;
+                        switch(choice){
+                            case 1:
+                                tree.preorder();
+                                break;
+                            case 2:
+                                tree.inorder();
+                                break;
+                            case 3:
+                                tree.postorder();
+                                break;
+                            default:
+                                cout<<"Invalid choice!"<<endl;
+                        }
+                        cout<<"\n\n\tRun again traverse loop: 1-YES, 0:NO \n";
+                        cin>>ans;
+                    }while(ans==1);
+                }
+                break;
+            case 3:
+                if(tree.check()){
+                    cout<<"Tree is empty!\n";
+                }
+                else{
+                    do{
+                        cout<<"\t 1: Preorder \n\t 2: Inorder \n\t 3: Postorder \n"<<endl;
+                        cin>>choice;
+                        switch(choice){
+                            case 1:
+                                tree.nonrec_preorder();
+                                break;
+                            case 2:
+                                tree.nonrec_inorder();
+                                break;
+                            case 3:
+                                tree.nonrec_postorder();
+                                break;
+                            default:
+                                cout<<"Invalid choice!"<<endl;
+                        }
+                        cout<<"\n\n\tRun again traverse loop: 1-YES, 0:NO \n";
+                        cin>>ans;
+                    }while(ans==1);
+                }
+                break;
+            default:
+                cout<<"Invalid Choice! \n";
+        }
+        cout<<"Run again Program : 1-YES, 0:NO ";
+        cin>>ans;
+    }while(ans==1);
+    return 0;
+}
